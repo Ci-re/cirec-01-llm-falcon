b0e0127b587df0913e579be5bc78fe44548a03b7
Ci-re <cirecleen@gmail.com>
2025-01-22 16:51:08 +0100

    activity: streamlit fully functional

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ HOST_URL = "http://localhost:9200"
 if "id" not in st.session_state:
     st.session_state.id = uuid.uuid4()
     st.session_state.file_cache = {}
-    
+
 session_id = st.session_state.id
 client = None
 
@@ -25,29 +25,42 @@ def reset_chat():
 
 es = Elasticsearch(HOST_URL)
 data = load_and_prepare_data()
-    
-try:
-    logging.info("Trying VectorSearch")
-    vs = VectorSearch(data=data, index_name="got_question_v",num_results=5, search_client=es)
-    vs.start_vs()
-except Exception as e:
-    logging.info("Vector search failed, trying Elastic Search")
-    els = AdvancedElasticSearch(data=data, index_name="got_question_e", num_results=5, search_client=es)
-    els.start_elastic_search()
-finally:
-    logging.info("Using Minsearch instead")
-    bs = BasicSearch(data=data, num_results=5)    
-    search_algo = bs.create_index()
-    
+
+# try:
+#     logging.info("Trying VectorSearch")
+#     # vs = VectorSearch(data=data, index_name="got_question_v",num_results=5, search_client=es)
+#     # vs.start_vs()
+#     bs = BasicSearch(data=data, num_results=5)
+#     search_algo = bs.create_index()
+
+# except Exception as e:
+#     logging.info(f"Vector search failed, trying Elastic Search {e}" )
+#     els = AdvancedElasticSearch(data=data, index_name="got_question_e", num_results=5, search_client=es)
+#     els.start_elastic_search()
+
+# finally:
+#     logging.info("Using Minsearch instead")
+#     bs = BasicSearch(data=data, num_results=5)
+#     search_algo = bs.create_index()
+
+
+bs = BasicSearch(data=data, num_results=5)
+search_algo = bs.create_index()
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
+
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-        
+
+
+def response_gen(streaming_response):
+    for word in streaming_response.split():
+        yield f"{word} "
+        time.sleep(0.05)
+
 if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -55,31 +68,24 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    def response_gen(streaming_response):    
-        for word in streaming_response.split():
-            yield f"{word} "
-            time.sleep(0.05)
-            
-
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        if not AdvancedElasticSearch.connect | VectorSearch.connect:
-            search_results = bs.basic_search(query=prompt)
-        search_results = vs.vector_search(query=prompt)
+        # if not AdvancedElasticSearch.connect | VectorSearch.connect:
+        search_results = bs.basic_search(query=prompt)
+        print(search_results[0])
+        # search_results = vs.vector_search(query=prompt)
 
         prompt = build_prompt(query=prompt, search_results=search_results)
         # Simulate stream of response with milliseconds delay
         streaming_response = llm(prompt)
 
-        for chunk in response_gen(streaming_response=streaming_response):
-            full_response = st.write_stream(response_gen(streaming_response=streaming_response))
-            full_response += chunk
-            message_placeholder.markdown(full_response + "▌")
+        full_response = st.write_stream(response_gen(streaming_response))
+            # full_response += chunk
+            # message_placeholder.markdown(f"{full_response}▌")
 
-        message_placeholder.markdown(full_response)
-        # st.session_state.context = ctx
+        # message_placeholder.markdown(full_response)
+            # st.session_state.context = ctx
 
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-
